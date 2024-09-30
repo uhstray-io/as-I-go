36a2d0f5ac342659489331729f8058f0ed68f1aa
Joseph A. Wisneski IV <stray@uhstray.io>
2024-09-29 20:47:37 -0400

    added first py/go files

---
+++ first.py
@@ -0,0 +1,32 @@
+# Set a goal
+## Create a seed of thought
+class seed():
+    name = str(),
+    scope = str(),
+    purpose = str()
+
+def __init__(self, name, scope, purpose):
+    self.name = name
+    self.scope = scope
+    self.purpose = purpose
+
+def  create_seed(self):
+    self.name = input("What is the name of your seed?")
+    self.scope = input("What is the scope of your seed? (Scope defines the boundaries of the code you'd like to write.)")
+    self.purpose = input("What is the purpose of your seed?")
+    return self
+
+
+## Plant the seed on your map
+
+## Water the seed with your thoughts (your completed goals)
+### Define the scope of your seed
+### What is the purpose of your seed?
+### Write the code that makes it
+
+# Connect to a larger idea
+## Develop a game that will help everyone learn cognitive behavioral therapy, mindfulness, and software coding techniques that helps them achieve their dreams and goals.
+
+### Create a dream map
+
+
